1235799f62b68da2b6b250283fb62861bd63f9fb
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2021-08-12 12:12:51 +0400

    Configure tailwind

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './custom.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
+const Root = () => {
+  return (
+    // <Provider store={store}>
+    //   <PersistGate loading={null} persistor={persistor}>
+    //     <BrowserRouter>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    //     </BrowserRouter>
+    //   </PersistGate>
+    // </Provider>
+  );
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<Root />, document.getElementById('root'));
