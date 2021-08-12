5ecec0f693470dfc72d102b2827cfc34141408af
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2021-08-13 00:28:27 +0400

    Make the main page items brighter on hover

--- src/pages/Main.page.js
+++ src/pages/Main.page.js
@@ -4,25 +4,13 @@ import { Link } from 'react-router-dom';
 function MainPage() {
   return (
     <>
-      <div class="slider-container">
-        <div class="box sunrise">
-          <a href="#box2"></a>
-        </div>
-        <div class="slider">
-          <div class="box sunset">
-            <a href="#box1"></a>
-          </div>
-        </div>
+      <div className="slider-container">
+        <Link className="box sunrise" to="/creatures"></Link>
+        <Link className="slider" to="/characters">
+          <div className="box sunset"></div>
+        </Link>
       </div>
     </>
-    // <div className="flex w-full">
-    //   <Link to="/creatures" className="flex-1">
-    //     <div className="creatures_cover bg-red-700 h-screen">creatures</div>
-    //   </Link>
-    //   <Link to="/characters" className="flex-1">
-    //     <div className="characters_cover bg-blue-700 h-screen">characters</div>
-    //   </Link>
-    // </div>
   );
 }
 
