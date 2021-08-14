90b34725f16f3dbab5cac1543eece663c71b34f3
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2021-08-14 05:21:21 +0400

    Get rid of horizontal scrollbar

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -7,8 +7,8 @@ export default function Slider({ slides }) {
   const slidesArr =
     slides.length &&
     slides.map((el) => (
-      <div className="card bg-gray-900 flex flex-col items-center">
-        <h2 className="text-2xl font_courgette">{el.name}</h2>
+      <div className="card bg-gray-900 flex flex-col items-center relative">
+        <h2 className="text-2xl font_courgette">{el.class}</h2>
         <img
           className="object-contain w-full h-full"
           src={el.image}
@@ -18,10 +18,12 @@ export default function Slider({ slides }) {
     ));
 
   return (
-    <div className="loadingPage h-screen relative">
-      <div className="carousel_container">
-        <Carousel slides={slidesArr} autoplay={false} interval={1000} />
-      </div>
+    <div className="h-screen overflow-hidden">
+      {slidesArr.length && (
+        <div className="carousel_container">
+          <Carousel slides={slidesArr} autoplay={false} interval={1000} />
+        </div>
+      )}
     </div>
   );
 }
